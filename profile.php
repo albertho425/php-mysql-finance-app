531d16a1e80c509d6dd1cb6abcb83e0891b16af5
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2022-04-02 22:29:48 -0700

    Put repeating code into template file

--- profile.php
+++ profile.php
@@ -42,28 +42,8 @@ if (isset($_POST['but_upload'])) {
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Expense Manager - Dashboard</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/style.css" rel="stylesheet">
-
-    <!-- Feather JS for Icons -->
-    <script src="js/feather.min.js"></script>
-
-</head>
+<?php include "templateHtmlCssJs.php" ?>
 
 <body>
 
