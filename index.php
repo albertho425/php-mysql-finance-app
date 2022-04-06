c6d9dd5915cab9ee467299842963556d919a36d3
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2022-04-05 21:29:28 -0700

    Add pagination

--- index.php
+++ index.php
@@ -219,14 +219,36 @@ if (!$link) {
 }
 
 
-//Why does month function work in phpMyAdmin but not in PHP
-// $sql = "SELECT month(expensedate), expensecategory, expense from expenses WHERE user_id = '$userid'";
+
+if (isset($_GET['page_no']) && $_GET['page_no']!="") {
+  $page_no = $_GET['page_no'];
+} else {
+  $page_no = 1;
+  }
+
+$total_records_per_page = 3;
+$offset = ($page_no-1) * $total_records_per_page;
+$previous_page = $page_no - 1;
+$next_page = $page_no + 1;
+$adjacents = "2";
+
+$result_count = mysqli_query(
+  $con,
+  "SELECT COUNT(*) AS total_records FROM expenses"
+  );
+  $total_records = mysqli_fetch_array($result_count);
+  $total_records = $total_records['total_records'];
+  $total_no_of_pages = ceil($total_records / $total_records_per_page);
+  $second_last = $total_no_of_pages - 1; // total pages minus 1
+
+$total_records = $total_records['total_records'];
+$total_no_of_pages = ceil($total_records / $total_records_per_page);
+$second_last = $total_no_of_pages - 1; // total pages minus 1
 
 
-
-$sql = "SELECT expensedate, expensecategory, expense, expensename from expenses WHERE user_id = '$userid'";
-
+$sql = "SELECT expensedate, expensecategory, expense, expensename from expenses WHERE user_id = '$userid' LIMIT $offset, $total_records_per_page";
 $result = mysqli_query($link, $sql);
+
 
 if (mysqli_num_rows($result) > 0) {
   // output table header
@@ -251,15 +273,48 @@ if (mysqli_num_rows($result) > 0) {
 } else {
   echo "0 results";
 }
+
+
 $link->close();
 
 ?>
+    <!-- pagination -->
+    <div style='padding: 10px 20px 0px; border-top: dotted 1px #CCC;'>
+      <strong>Page <?php echo $page_no." of ".$total_no_of_pages; ?></strong>
+    </div>
+
+    <ul class="pagination">
+      <?php if($page_no > 1){
+      echo "<li><a href='?page_no=1'>First Page</a></li>";
+      } ?>
+          
+      <li <?php if($page_no <= 1){ echo "class='disabled'"; } ?>>
+      <a <?php if($page_no > 1){
+      echo "href='?page_no=$previous_page'";
+      } ?>>Previous</a>
+      </li>
+          
+      <li <?php if($page_no >= $total_no_of_pages){
+      echo "class='disabled'";
+      } ?>>
+      <a <?php if($page_no < $total_no_of_pages) {
+      echo "href='?page_no=$next_page'";
+      } ?>>Next</a>
+      </li>
+
+      <?php if($page_no < $total_no_of_pages){
+      echo "<li><a href='?page_no=$total_no_of_pages'>Last &rsaquo;&rsaquo;</a></li>";
+      } ?>
+  </ul>
+
 </div>
     </div>
     <!-- /#page-content-wrapper -->
 
+    
   </div>
   <!-- /#wrapper -->
+  
 
 </body>
 
--- manage_expense.php
+++ manage_expense.php
@@ -78,6 +78,44 @@ $exp_fetched = mysqli_query($con, "SELECT * FROM expenses WHERE user_id = '$user
                                 </tr>
                             </thead>
 
+                            
+                            <?php
+
+                            $link = mysqli_connect("localhost","root","root","dailyexpense");
+                            if (!$link) {
+                            die("Connection failed: " . mysqli_connect_error());
+                            }
+
+                                if (isset($_GET['page_no']) && $_GET['page_no']!="") {
+                                    $page_no = $_GET['page_no'];
+                                } else {
+                                    $page_no = 1;
+                                    }
+
+                                $total_records_per_page = 3;
+                                $offset = ($page_no-1) * $total_records_per_page;
+                                $previous_page = $page_no - 1;
+                                $next_page = $page_no + 1;
+                                $adjacents = "2";
+
+                                $result_count = mysqli_query(
+                                $con,
+                                "SELECT COUNT(*) AS total_records FROM expenses"
+                                );
+                                $total_records = mysqli_fetch_array($result_count);
+                                $total_records = $total_records['total_records'];
+                                $total_no_of_pages = ceil($total_records / $total_records_per_page);
+                                $second_last = $total_no_of_pages - 1; // total pages minus 1
+
+                                $total_records = $total_records['total_records'];
+                                $total_no_of_pages = ceil($total_records / $total_records_per_page);
+                                $second_last = $total_no_of_pages - 1; // total pages minus 1
+
+
+                                $sql = "SELECT expensedate, expensecategory, expense, expensename from expenses WHERE user_id = '$userid' LIMIT $offset, $total_records_per_page";
+                                $result = mysqli_query($link, $sql);
+                            ?>
+
                             <?php $count=1; while ($row = mysqli_fetch_array($exp_fetched)) { ?>
                                 <tr>
                                     <td><?php echo $count;?></td>
@@ -95,15 +133,55 @@ $exp_fetched = mysqli_query($con, "SELECT * FROM expenses WHERE user_id = '$user
                             <?php $count++; } ?>
                         </table>
                     </div>
-
+            
                 </div>
             </div>
-        </div>
+
+
+    <!-- Number of pages -->
+    <div style='padding: 10px 20px 0px; border-top: dotted 1px #CCC;'>
+        <strong>Page <?php echo $page_no." of ".$total_no_of_pages; ?></strong>
+    </div>
+
+    <!-- Pagination-->                                
+    <ul class="pagination">
+        <?php if($page_no > 1){
+        echo "<li><a href='?page_no=1'>First Page</a></li>";
+        } ?>
+            
+        <li <?php if($page_no <= 1){ echo "class='disabled'"; } ?>>
+        <a <?php if($page_no > 1){
+        echo "href='?page_no=$previous_page'";
+        } ?>>Previous</a>
+        </li>
+            
+        <li <?php if($page_no >= $total_no_of_pages){
+        echo "class='disabled'";
+        } ?>>
+        <a <?php if($page_no < $total_no_of_pages) {
+        echo "href='?page_no=$next_page'";
+        } ?>>Next</a>
+        </li>
+
+        <?php if($page_no < $total_no_of_pages){
+        echo "<li><a href='?page_no=$total_no_of_pages'>Last &rsaquo;&rsaquo;</a></li>";
+        } ?>
+    </ul>
+        
+    </div>
+
+
+
+            
+
     </div>
     <!-- /#page-content-wrapper -->
 
+    
+
     </div>
     <!-- /#wrapper -->
+
 
     <!-- Bootstrap core JavaScript -->
     <script src="js/jquery.slim.min.js"></script>
