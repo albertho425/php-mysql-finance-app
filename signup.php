13430a95d2a8d72d490be23992edfe18caabd388
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2022-04-09 23:18:58 -0700

    Update CSS styling of sign up form

---
+++ signup.php
@@ -0,0 +1,86 @@
+<?php
+require('config.php');
+if (isset($_REQUEST['firstname'])) {
+  if ($_REQUEST['password'] == $_REQUEST['confirm_password']) {
+    $firstname = stripslashes($_REQUEST['firstname']);
+    $firstname = mysqli_real_escape_string($con, $firstname);
+    $lastname = stripslashes($_REQUEST['lastname']);
+    $lastname = mysqli_real_escape_string($con, $lastname);
+
+    $email = stripslashes($_REQUEST['email']);
+    $email = mysqli_real_escape_string($con, $email);
+
+
+    $password = stripslashes($_REQUEST['password']);
+    $password = mysqli_real_escape_string($con, $password);
+
+
+    $trn_date = date("Y-m-d H:i:s");
+
+    $query = "INSERT into `users` (firstname, lastname, password, email, trn_date) VALUES ('$firstname','$lastname', '" . md5($password) . "', '$email', '$trn_date')";
+    $result = mysqli_query($con, $query);
+    if ($result) {
+      header("Location: login.php");
+    }
+  } else {
+    echo ("ERROR: Please Check Your Password & Confirmation password");
+  }
+}
+?>
+
+<?php include "templateHtmlCssJs.php" ?>
+<?php template_header("Register");?>
+<!-- loads custom styling -->
+<link href="css/signup.css" rel="stylesheet">
+
+<body>
+  <div class="signup-form">
+    <form action="" method="POST" autocomplete="off">
+    <h3 class="text-center">Sign Up</h3>
+      <div class="col text-center"><br>
+            <a href="manage_expense.php"><img src="icon/maex.png" width="57px" /></a>
+        </div><br>
+      <div class="form-group">
+        <div class="row">
+          <div class="col"><input type="text" class="form-control" name="firstname" placeholder="First Name" required="required"></div>
+          <div class="col"><input type="text" class="form-control" name="lastname" placeholder="Last Name" required="required"></div>
+        </div>
+      </div>
+      <div class="form-group">
+        <input type="email" class="form-control" name="email" placeholder="Email" required="required">
+      </div>
+      <div class="form-group">
+        <input type="password" class="form-control" name="password" placeholder="Password" required="required">
+      </div>
+      <div class="form-group">
+        <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" required="required">
+      </div>
+      <div class="form-group">
+        <label class="form-check-label"><input type="checkbox" required="required"> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
+      </div>
+      <div class="form-group">
+        <button type="submit" class="btn btn-success btn-lg" style="border-radius:0%;">Register</button>
+      </div>
+    </form>
+    <div class="text-center">Already have an account? <a class="text-success" href="login.php">Login Here</a></div>
+  </div>
+  <?php display_footer();?>
+
+</body>
+<!-- Bootstrap core JavaScript -->
+<script src="js/jquery.slim.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Croppie -->
+<script src="js/profile-picture.js"></script>
+<!-- Menu Toggle Script -->
+<script>
+  $("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+  });
+</script>
+<script>
+  feather.replace()
+</script>
+
+</html>
